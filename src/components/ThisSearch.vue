d21822b7a1f360e7f8762aeb185ccc5d0fb3c870
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-03-31 15:32:13 +0200

    inserimento barra ricerca e vutton

---
+++ src/components/ThisSearch.vue
@@ -0,0 +1,21 @@
+<script>
+import { store } from '../store.js'
+export default {
+    name: 'ThisSearch',
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="containerSearch">
+        <input type="text" placeholder="Cerca Film">
+        <button>Search</button>
+
+    </div>
+</template>
+
+<style></style>
