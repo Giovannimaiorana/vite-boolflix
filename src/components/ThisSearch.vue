6803ad2df7bef232de6411a9ad58151b0bf0917e
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-04-01 03:37:49 +0200

    aggiunta list in jumbotron

--- src/components/ThisSearch.vue
+++ src/components/ThisSearch.vue
@@ -14,17 +14,33 @@ export default {
     <div class="containerHeader">
         <div class="containerLogo">
             <img src="/logo.png" alt="">
+            <ul>
+                <li>Home</li>
+                <li>Serie Tv</li>
+                <li>Film</li>
+                <li>Nuovi e Popolari</li>
+                <li>La Mia Lista</li>
+                <li>Sfoglia per Lingua</li>
+
+            </ul>
         </div>
         <div class="containerSearch">
             <input type="text" placeholder="Cerca Film" v-model="store.search">
             <button @click="$emit('searchText')"><i class="fa-solid fa-magnifying-glass"></i></button>
 
         </div>
+        <div class="containerinfo">
+            <div class="logotext">
+                <img src="/logosearch.png" alt="">
+
+            </div>
+        </div>
     </div>
 </template>
 
 <style lang="scss">
 .containerHeader {
+
     display: flex;
     justify-content: space-between;
     align-items: flex-start;
@@ -32,8 +48,46 @@ export default {
     background-size: cover;
     height: 500px;
 
+    .logotext {
+        display: flex;
+        align-items: center;
+    }
+
+    .containerinfo {
+        width: 700px;
+        height: 350px;
+        position: absolute;
+        left: 20px;
+        top: 100px;
+        border: 1px solid black;
+
+        img {
+            width: 60px;
+
+        }
+    }
+
     .containerLogo {
         margin-top: 20px;
+        display: flex;
+
+        ul {
+            list-style: none;
+            display: flex;
+            align-items: center;
+            margin-left: 10px;
+
+            li {
+                margin-left: 20px;
+                color: white;
+                cursor: pointer;
+            }
+
+            li:hover {
+                color: black;
+            }
+        }
+
 
         img {
             width: 200px;
@@ -53,6 +107,7 @@ export default {
             border: 1px solid grey;
             margin-right: 20px;
             padding: 10px;
+            color: grey;
         }
 
         button {
